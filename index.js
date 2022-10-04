a71e5c71ed795452b94e9675eebd2b4591aaa58d
Thomas Heck <dutchfence@gmail.com>
2022-10-04 17:27:24 +0200

    Main page is done for now
    It just needs a womans touch for the styling

--- index.js
+++ index.js
@@ -20,18 +20,24 @@ const commentSchema = new mongoose.Schema({
 
 const articleSchema = new mongoose.Schema({
   title: String,
+  tag: String,
   description: String,
   thumbnail: {type:String, default: "https://images0.persgroep.net/rcs/fQHfEbeRmmxMtdsOCz3LO4IfYqE/diocontent/112687658/_crop/0/0/1580/893/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8&desiredformat=webp"},
   article: String,
+  date: String,
   views: {type:Number, default: 0},
-  comments: [commentSchema]
+  comments: [commentSchema],
+  location: {type: String, default: ""}
 });
 const titles=[
 ]
+const locations=[];
+const tags=[];
 const descriptionsRaw=[]
 const pictures =[];
 let descriptions =[];
 const articles =[];
+const dates =[];
 const Article = mongoose.model("Article", articleSchema);
 
  Article.find((err, blogPosts)=>{
@@ -40,12 +46,14 @@ const Article = mongoose.model("Article", articleSchema);
   } else {
 
  blogPosts.forEach((blog)=>{
-console.log(blog.title + "blogggs");
    titles.unshift(blog.title),
    descriptionsRaw.unshift(blog.description),
    pictures.unshift(blog.thumbnail),
-   articles.unshift(blog.article)
-   console.log(descriptionsRaw);
+   tags.unshift(blog.tag),
+   dates.unshift(blog.date),
+   articles.unshift(blog.article),
+   locations.unshift(blog.location)
+
  });
 descriptions = descriptionsRaw.map(x => _.truncate(x, {
    "length": 300,
@@ -54,7 +62,7 @@ descriptions = descriptionsRaw.map(x => _.truncate(x, {
 
   }
 });
-console.log("uit de loop" + descriptions);
+
 
 
 const blogPosts = ''
@@ -63,8 +71,11 @@ app.get("/", (req,res)=>{
   res.render("index", {
   pictures: pictures,
   titles: titles,
+  tags: tags,
   description: descriptions,
-  article: articles
+  article: articles,
+  dates:dates,
+  locations: locations
 
   });
 })
@@ -75,16 +86,28 @@ app.get("/upload", (req,res) =>{
 })
 
 app.post("/upload", (req, res)=>{
+
+  const time = new Date();
+  const date = time.toLocaleDateString("nl-NL", {
+    weekday: "long",
+    day: "numeric",
+    month:"long"
+  })
   const title = req.body.Title;
   const description = req.body.Description;
   const article = req.body.Article;
   const thumbnail = req.body.Thumbnail;
-  console.log(title, description, article);
+  const tag = req.body.Tag;
+  const location = req.body.Location
+
   const newPost = new Article({
     title: title,
+    tag: tag,
     description: description,
     article: article,
-    thumbnail: thumbnail
+    thumbnail: thumbnail,
+    date: date,
+    location: location
   });
   newPost.save();
   console.log("saved sucesfully!");
