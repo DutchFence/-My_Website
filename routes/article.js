dcdd41d49519064472c2d81b31f847a7b97532f5
Thomas Heck <dutchfence@gmail.com>
2022-10-07 17:22:23 +0200

    Seperated the model and schemas
    now want to make a file with search functions

---
+++ routes/article.js
@@ -0,0 +1,25 @@
+module.exports = function(mongoose){
+
+  let commentSchema = new mongoose.Schema({
+    commentator : String,
+    comment : String,
+    email : {type:String, default:"not given"}
+  });
+
+  let articleSchema = new mongoose.Schema({
+    title: String,
+    tag: String,
+    description: String,
+    thumbnail: {type:String, default: "https://images0.persgroep.net/rcs/fQHfEbeRmmxMtdsOCz3LO4IfYqE/diocontent/112687658/_crop/0/0/1580/893/_fitwidth/763?appId=93a17a8fd81db0de025c8abd1cca1279&quality=0.8&desiredformat=webp"},
+    article: String,
+    date: String,
+    views: {type:Number, default: 0},
+    comments: [commentSchema],
+    location: {type: String, default: ""}
+  });
+
+  var models={
+    Article: mongoose.model("Article", articleSchema)
+  }
+  return models;
+}
