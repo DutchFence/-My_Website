bf3776b33be7ca8085fe00582d9a9031f767a199
Thomas Heck <dutchfence@gmail.com>
2022-10-10 12:17:39 +0200

    Another one
    And such a small one with big effects, the static dir for public has to be declared before views ejs otherwise it will be linked into the req.params

--- index.js
+++ index.js
@@ -4,13 +4,19 @@ const ejs = require("ejs");
 const app = express();
 const bodyparser= require("body-parser");
 const mongoose=require("mongoose");
-
+const homeRouter = require("./routes/home.js");
+const uploadRouter = require("./routes/upload.js");
+const articleRouter = require("./routes/articles.js");
 
 app.use(bodyparser.urlencoded({ extended: true }));
-app.set('view engine', 'ejs');
 app.use(express.static(__dirname + '/public'));
+app.set('view engine', 'ejs');
+
 
 mongoose.connect("mongodb://localhost:27017/blogPosts");
+app.use("/",  homeRouter);
+app.use("/upload", uploadRouter);
+app.use("/article", articleRouter);
 
 
 
@@ -18,9 +24,3 @@ mongoose.connect("mongodb://localhost:27017/blogPosts");
 app.listen(3000, ()=>{
   console.log("server 3000 online");
 })
-const homeRouter = require("./routes/home.js");
-const uploadRouter = require("./routes/upload.js");
-const articleRouter = require("./routes/articles.js");
-app.use("/",  homeRouter);
-app.use("/upload", uploadRouter);
-app.use("/article", articleRouter);
